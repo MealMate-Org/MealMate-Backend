08504a7bc7828d7da6d6c4cc58559ea485a61415
miguelbac <mbacebal@gmail.com>
2025-10-07 15:15:21 +0200

    Initial commit

---
+++ src/main/java/com/MealMate/MealMateBackend/MealMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.MealMate.MealMateBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealMateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MealMate/MealMateBackend/MealMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MealMate.MealMateBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
